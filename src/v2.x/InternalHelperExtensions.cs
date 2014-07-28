1781048222b87768490923c1e343a8e6ceebf184
Oliver Baker <oliver.baker@sixnetworks.net>
2014-07-29 01:32:27 +0200

    Fix for older CookieContainers. Failing in Mono

--- src/v2.x/InternalHelperExtensions.cs
+++ src/v2.x/InternalHelperExtensions.cs
@@ -12,38 +12,50 @@ namespace Microsoft.AspNet.SignalR.Client
     {
 
         internal static CookieCollection GetAllCookies(this CookieContainer cookieJar) {
-            CookieCollection cookieCollection = new CookieCollection();
 
-            Hashtable table = (Hashtable)cookieJar.GetType().InvokeMember("m_domainTable",
-                                                                            BindingFlags.NonPublic |
-                                                                            BindingFlags.GetField |
-                                                                            BindingFlags.Instance,
-                                                                            null,
-                                                                            cookieJar,
-                                                                            new object[] { });
+            try {
+                var obj = (CookieCollection) cookieJar.GetType().InvokeMember("cookies", BindingFlags.NonPublic |
+                                                                                         BindingFlags.GetField |
+                                                                                         BindingFlags.Instance,
+                    null,
+                    cookieJar,
+                    new object[] {});
+                return obj;
+            } catch {
+                CookieCollection cookieCollection = new CookieCollection();
 
-            foreach (var tableKey in table.Keys) {
-                String str_tableKey = (string)tableKey;
 
-                if (str_tableKey[0] == '.') {
-                    str_tableKey = str_tableKey.Substring(1);
+                Hashtable table = (Hashtable)cookieJar.GetType().InvokeMember("m_domainTable",
+                                                                                BindingFlags.NonPublic |
+                                                                                BindingFlags.GetField |
+                                                                                BindingFlags.Instance,
+                                                                                null,
+                                                                                cookieJar,
+                                                                                new object[] { });
+
+                foreach (var tableKey in table.Keys) {
+                    String str_tableKey = (string)tableKey;
+
+                    if (str_tableKey[0] == '.') {
+                        str_tableKey = str_tableKey.Substring(1);
+                    }
+
+                    SortedList list = (SortedList)table[tableKey].GetType().InvokeMember("m_list",
+                                                                                BindingFlags.NonPublic |
+                                                                                BindingFlags.GetField |
+                                                                                BindingFlags.Instance,
+                                                                                null,
+                                                                                table[tableKey],
+                                                                                new object[] { });
+
+                    foreach (var listKey in list.Keys) {
+                        String url = "https://" + str_tableKey + (string)listKey;
+                        cookieCollection.Add(cookieJar.GetCookies(new Uri(url)));
+                    }
                 }
 
-                SortedList list = (SortedList)table[tableKey].GetType().InvokeMember("m_list",
-                                                                            BindingFlags.NonPublic |
-                                                                            BindingFlags.GetField |
-                                                                            BindingFlags.Instance,
-                                                                            null,
-                                                                            table[tableKey],
-                                                                            new object[] { });
-
-                foreach (var listKey in list.Keys) {
-                    String url = "https://" + str_tableKey + (string)listKey;
-                    cookieCollection.Add(cookieJar.GetCookies(new Uri(url)));
-                }
+                return cookieCollection;
             }
-
-            return cookieCollection;
         }
 
     }
